42e66c743d5dc725e8ddd41bf45104b34681a172
alan6584 <alanwang6584@gmail.com>
2017-11-24 23:46:48 +0800

    提交字符串格式化输出的练习代码

---
+++ demos/D007.py
@@ -0,0 +1,17 @@
+#! /usr/bin/python
+# -*- coding: UTF-8 -*-
+
+print 'my name is %s and weight is %d kg!' % ('Alan', 65)
+
+a = 3.0
+print 'a = %f' % a
+
+a = 9
+print 'a = %d, a(8) = %o' % (a, a)
+
+a = 18
+print 'a = %d, a(16) = %x' % (a, a)
+
+
+
+
