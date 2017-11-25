256978b3a746e016d3abd5bb8791e9ba9ff2c57e
alan6584 <alanwang6584@gmail.com>
2017-11-25 23:24:04 +0800

    提交字符串的代码

--- demos/D007.py
+++ demos/D007.py
@@ -3,6 +3,11 @@
 
 print 'my name is %s and weight is %d kg!' % ('Alan', 65)
 
+str = 'my name is Alan and weight is 65kg!'
+print str.find('Alan')
+isHaveTheStr = (str.find('Alan') > -1)
+print isHaveTheStr
+
 a = 3.0
 print 'a = %f' % a
 
