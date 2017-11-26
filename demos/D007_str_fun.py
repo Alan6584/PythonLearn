7b42dfe8e4f926ee9a34780182a030eda6fa47cf
alan6584 <alanwang6584@gmail.com>
2017-11-26 18:10:05 +0800

    提交字符串相关函数的练习代码

--- demos/D007_str_fun.py
+++ demos/D007_str_fun.py
@@ -16,7 +16,7 @@ print 'Encoded str :', str
 str = str.decode('UTF-8', 'strict')
 print 'Decoded str :', str 
 
-
+print '-------------------------------------'
 str = 'Hello my name is Alan, weight is 65kg'
 print str.startswith('Hello')
 print str.endswith('kg')
@@ -24,7 +24,7 @@ print str.find('Alan')
 print str.isalnum() #如果str中至少有一个字符并且所有字符都是字母或数字则返回True否则返回False
 print str.isalpha() #如果str中至少有一个字符并且所有字符都是字母则返回True否则返回False
 
-
+print '-------------------------------------'
 print str.isdigit() # 如果str中只包含数字则返回True
 a = '12345'
 print a.isdigit()
@@ -33,4 +33,28 @@ list = ['a', 'b', 'c', 'd']
 print list
 a = '-'
 print a.join(list) # 以 string 作为分隔符，将 seq 中所有的元素(的字符串表示)合并为一个新的字符串
+
+print '-------------------------------------'
+print str.lower()
+print str.upper()
+print str.title() #返回"标题化"的 string,就是说所有单词都是以大写开始，其余字母均为小写
+
+print '-------------------------------------'
+print str.split(',') #以 str 为分隔符切片 string
+
+print '-------------------------------------'
+a = '   hello world!  '
+print a
+print a.strip()
+print a.lstrip()
+print a.rstrip()
+
+print '-------------------------------------'
+a = '***hello world!***'
+print a
+print a.strip('*')
+print a.lstrip('*')
+print a.rstrip('*')
+
+
 
