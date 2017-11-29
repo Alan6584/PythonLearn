edda87913a2fd81c614e98960812a236f45999f0
alan6584 <alanwang6584@gmail.com>
2017-11-29 08:39:34 +0800

    提交字典练习代码

--- demos/D010_dictionary.py
+++ demos/D010_dictionary.py
@@ -18,3 +18,28 @@ print "dict.items() =",dict.items()
 
 print "dict.keys() =",dict.keys()
 print "dict.values() =",dict.values()
+
+print '--------------------------------'
+for key,value in dict.items():
+	print ("key = " + key + " , value = " + value)
+
+print '--------------------------------'
+users = {
+	"Alan":{
+		"id":"0001",
+		"email":"alanwang6584@gmail.com",
+		"location":"HB"
+	},
+	"Jajuan":{
+		"id":"0002",
+		"email":"jajuan@gmail.com",
+		"location":"BJ"
+	}
+}
+
+print users
+
+print '--------------------------------'
+for username,userinfo in users.items():
+	print("username:" + username + "\nuserId:" + userinfo["id"] + "\nuserEmail:" + userinfo["email"] + "\nuserLocation:" + userinfo["location"])
+	print '--------------------------------'
