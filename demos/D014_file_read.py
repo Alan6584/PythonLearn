ae3505c22c646ebffadad0c2494481d008a5f21d
alan6584 <alanwang6584@gmail.com>
2017-12-04 19:27:58 +0800

    提交文件比较及练习代码

--- demos/D014_file_read.py
+++ demos/D014_file_read.py
@@ -19,8 +19,17 @@ fp.seek(0, 0)
 
 str = fp.read(100)
 print str
-
 print fp.tell()
+
+print "-------------"
+fp.seek(0, 0)
+str = ""
+while(True):	
+	str = fp.readline()
+	if(str == ""):
+		break
+
+
 print os.getcwd()
 
 fp.close()
