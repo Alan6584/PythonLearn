0643cd82d42692842d26407d94532150a57a063b
alan6584 <alanwang6584@gmail.com>
2017-12-04 13:46:00 +0800

    提交文件操作的练习代码

--- demos/D014_file_read.py
+++ demos/D014_file_read.py
@@ -4,7 +4,20 @@
 import os
 
 fp = open("./test/D014_file.txt", "rb")
-str = fp.read(60)
+
+str = fp.readline()
+print str
+print fp.tell()
+print "-------------"
+
+str = fp.readline()
+print str
+print fp.tell()
+print "-------------"
+
+fp.seek(0, 0)
+
+str = fp.read(100)
 print str
 
 print fp.tell()
--- demos/D014_file_write.py
+++ demos/D014_file_write.py
@@ -5,5 +5,7 @@ fp = open("./test/D014_file.txt", "wb")
 
 fp.write("My name is Alan.\n")
 fp.write("My email is alanwang6584@gmail.com\n")
+str = "Hello Alan!\nWelcome Python!\n"
+fp.writelines(str)
 
 fp.close()
