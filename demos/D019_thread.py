7efbccce053b568ce8f2e4c004d40dd30d62879e
alan6584 <alanwang6584@gmail.com>
2017-12-19 21:26:11 +0800

    提交多线程练习代码

--- demos/D019_thread.py
+++ demos/D019_thread.py
@@ -13,6 +13,14 @@ class MyThread(threading.Thread):
 
 	def run(self):
 		print "MyThread:%d -->run start......count:%d" % (self.threadID, self.count)
+		self.__loop(self.count)
+		print "MyThread:%d -->run end......" % (self.threadID)
+
+	def __loop(self, count):
+		while (count > 0):
+			time.sleep(1) #睡眠1秒
+			print "MyThread:%d -->loop()......count:%d" % (self.threadID, count)
+			count -= 1
 
 t1 = MyThread(1, 5)
 t2 = MyThread(2, 6)
