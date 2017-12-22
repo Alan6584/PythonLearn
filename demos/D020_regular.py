6bec95e5c711ec21a157761c9e2f3d7b23eca7b4
alan6584 <alanwang6584@gmail.com>
2017-12-22 09:16:13 +0800

    提交正则表达式练习代码

--- demos/D020_regular.py
+++ demos/D020_regular.py
@@ -3,9 +3,15 @@
 
 import re
 
-str = "010-12345"
+# str = "010-123" # OK
+# str = "010-12345" # OK
+str = "010-12345678" # OK
+# str = "010-123456789" # Failed
+# str = "0110-12345678" # O# Failed
 
+# 匹配{三位数字}-{3到8位数字}
 ret = re.match(r'^\d{3}\-\d{3,8}$', str)
+
 if ret:
 	print "OK"
 else:
