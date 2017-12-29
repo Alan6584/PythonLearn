6d6db6b65f43b6067a8856ae746afca964ae555d
alan6584 <alanwang6584@gmail.com>
2017-12-29 17:29:19 +0800

    提交GUI练习代码

---
+++ demos/D022_gui.py
@@ -0,0 +1,22 @@
+#! /usr/bin/python
+# -*- coding:UTF-8 -*-
+
+from Tkinter import *
+
+class Application(Frame):
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.pack()
+        self.createWidgets()
+
+    def createWidgets(self):
+        self.helloLabel = Label(self, text='Hello, world!')
+        self.helloLabel.pack()
+        self.quitButton = Button(self, text='Quit', command=self.quit)
+        self.quitButton.pack()
+
+
+app = Application()
+app.master.title("Test")
+app.mainloop()
+
