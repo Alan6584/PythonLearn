839c679917c434f799747f757db7b35ed6f42d37
alan6584 <alanwang6584@gmail.com>
2018-11-10 20:10:28 +0800

    修改 OpenCV 测试代码

--- opencv/cv_readimage.py
+++ opencv/cv_readimage.py
@@ -6,15 +6,14 @@
 @Email   : alanwang6584@gmail.com
 """
 
-
 import numpy as np
-import cv2
+import cv2 as cv
  
-# img = cv2.imread('/Users/wangjianjun/Desktop/Debug/pics/aaa.jpeg', cv2.IMREAD_UNCHANGED) # 按原始颜色格式读取图片
-img = cv2.imread('/Users/wangjianjun/Desktop/Debug/pics/aaa.jpeg', cv2.IMREAD_GRAYSCALE) # 按灰度图读取图片
-print img.shape
+img = cv.imread('/Users/wangjianjun/Desktop/Debug/pics/aaa.jpeg', cv.IMREAD_UNCHANGED) # 按原始颜色格式读取图片
+# img = cv.imread('/Users/wangjianjun/Desktop/Debug/pics/aaa.jpeg', cv.IMREAD_GRAYSCALE) # 按灰度图读取图片
+print img.shape # 输出：(240, 240, 3)
 
-cv2.namedWindow("image")
-cv2.imshow('image', img)
-cv2.waitKey (0)
-cv2.destroyAllWindows()
+cv.namedWindow("image")
+cv.imshow('image', img)
+cv.waitKey (0) # 如果没有这句显示后立马就消失了
+cv.destroyAllWindows()
